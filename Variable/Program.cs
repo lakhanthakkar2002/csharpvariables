9e5c8655887328cf77dd67f24908e7a635a9fe0b
mesaniyaasif <88967069+mesaniyaasif@users.noreply.github.com>
2024-02-28 10:57:19 +0530

    hello world

--- Variable/Program.cs
+++ Variable/Program.cs
@@ -20,6 +20,8 @@ namespace Variable
             }
             int currentNum = previousNum + nextNum;
             Console.WriteLine(currentNum);
+            Console.WriteLine("Hello World");
+
             
             previousNum = nextNum;
             nextNum= currentNum;
