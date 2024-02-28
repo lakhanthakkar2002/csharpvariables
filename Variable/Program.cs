111612e0edc5525a9b4c17f41c99ece0a33b1e94
mesaniyaasif <88967069+mesaniyaasif@users.noreply.github.com>
2024-02-28 11:11:11 +0530

    hello world 3
    --
    
    Co-Authored-By: Thakkar Lakhan Rajeshkumar <145996547+lakhanthakkar2002@users.noreply.github.com>
    Co-Authored-By: Lakhan Thakkar <60564938+lakhan20@users.noreply.github.com>

--- Variable/Program.cs
+++ Variable/Program.cs
@@ -23,6 +23,7 @@ namespace Variable
             Console.WriteLine("Hello World");
 
             Console.WriteLine("Hello World 2");
+            Console.WriteLine("Hello World 3");
 
 
             previousNum = nextNum;
