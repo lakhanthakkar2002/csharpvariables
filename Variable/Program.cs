76c2e4af09f8a60f3d801c7bd4b8df61465b097d
lakhanthakkar2002 <lakhanrthakkar22@gmail.com>
2024-02-28 10:45:29 +0530

    first commit

---
+++ Variable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+
+
+
+    internal class Program
+    {
+        void GenerateFibonacci(int previousNum,int nextNum,int lastNum)
+        {
+            if (nextNum >= lastNum)
+            {
+                return;
+            }
+            int currentNum = previousNum + nextNum;
+            Console.WriteLine(currentNum);
+            
+            previousNum = nextNum;
+            nextNum= currentNum;
+           GenerateFibonacci(previousNum,nextNum,lastNum);
+            Console.ReadLine();
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number until you want a fubonacci series : ");
+            int lastNum=Convert.ToInt32(Console.ReadLine());
+            int previousNum = 1;
+            int nextNum = 1;
+            Program program = new Program();
+            Console.WriteLine(previousNum);
+            Console.WriteLine(nextNum);
+
+            program.GenerateFibonacci(previousNum,nextNum,lastNum);
+                
+           
+        }
+    }
+}
